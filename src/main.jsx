281a5e3a480eec52022d9c3a01f3fca3e0b570ee
Leonardo Silva <leonardosivalima93@live.com>
2024-01-12 14:56:31 -0300

    estilo global

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 
+import theme from './styles/theme'
+
+import { Teste } from './pages/Teste'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Teste />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Teste/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+
+export function Teste() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Leonardo Silva</span>
+    </Container>
+  )
+}
