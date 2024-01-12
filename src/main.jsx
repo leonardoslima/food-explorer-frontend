58ea65002d0bbf4f87f952ff82b37345bd0a4643
Leonardo Silva <leonardosivalima93@live.com>
2024-01-12 16:46:46 -0300

    inicio da tela de SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Teste } from './pages/Teste'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Teste />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+
+  padding: 9.75rem 2.375rem;
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.LIGHT[100]};
+    font-size: 2rem;
+    font-weight: 700;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.625rem;
+  }
+`;
+
+export const Form = styled.form`
+  margin: 0 auto;
+  border-radius: 1.6rem;
+
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 2rem;
+`;
