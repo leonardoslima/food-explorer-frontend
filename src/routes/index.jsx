bcf09c75f9c00f9bdfb183037d425395246c6a15
Leonardo Silva <leonardosivalima93@live.com>
2024-01-24 10:24:44 -0300

    rota app

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { AuthRoutes } from './auth.routes';
+import { App } from './app.routes';
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <App />
     </BrowserRouter>
   );
 }
