80806cdd78224d9bca0dcf4f025cd15b6cd0db4c
Leonardo Silva <leonardosivalima93@live.com>
2024-04-23 15:17:49 -0300

    logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,20 @@
 import { List, MagnifyingGlass, Receipt, SignOut } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
+import { useAuth } from '../../hooks/auth';
+
 import { Container } from "./styles";
 
 import { Button } from '../Button'
 import { Input } from '../Input'
-import { Link } from "react-router-dom";
 
 
-export function Header({ onOpenMenu, $isAdmin = false }) {
+export function Header({ onOpenMenu }) {
+  const { signOut, user } = useAuth();
+
+  const isAdmin = user?.isAdmin || false;
+
   return (
-    <Container isAdmin={$isAdmin}>
+    <Container isAdmin={isAdmin}>
       <div>
         <button id="button-menu" onClick={onOpenMenu}>
           <List size={28} />
@@ -20,7 +26,7 @@ export function Header({ onOpenMenu, $isAdmin = false }) {
               <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#065E7C" />
             </svg>
             food explorer
-            {$isAdmin && <span>admin</span>}
+            {isAdmin && <span>admin</span>}
           </h1>
         </Link>
 
@@ -33,20 +39,22 @@ export function Header({ onOpenMenu, $isAdmin = false }) {
         </div>
 
         <Link
-          to={$isAdmin ? '/newDish' : ''}
+          to={isAdmin ? '/newDish' : ''}
           id="order-button"
         >
           <Button
-            title={$isAdmin ? 'Novo prato' : `Pedidos (${0})`}
-            icon={$isAdmin ? '' : <Receipt size={32} />}
+            title={isAdmin ? 'Novo prato' : `Pedidos (${0})`}
+            icon={isAdmin ? '' : <Receipt size={32} />}
           />
         </Link>
 
         <div id="logout">
-          <SignOut size={22} />
+          <button onClick={signOut}>
+            <SignOut size={22} />
+          </button>
         </div>
 
-        {!$isAdmin && (
+        {!isAdmin && (
           <button>
             <Receipt size={26} />
             <span>0</span>
